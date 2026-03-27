83f2da6898392d2b075991c374882c3dff9a2d68
shivam surati <shivamsurati04@gmail.com>
2026-03-27 16:14:06 +0530

    added get method

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const multer = require("multer");
 const uploadFile = require("./services/storage.service");
+const postModel = require("./models/post.model");
 app.use(express.json());
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -9,7 +10,22 @@ app.post("/create-post", upload.single("image"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
   const result = await uploadFile(req.file.buffer);
-  console.log(result);
+  const post = await postModel.create({
+    image: result.url,
+    caption: req.body.caption,
+  });
+  return res.status(201).json({
+    message: "Post created successfully!",
+    post,
+  });
+});
+
+app.get("/posts", async (req, res) => {
+  const posts = await postModel.find();
+  return res.status(200).json({
+    message: "Post fetched successfully!",
+    posts,
+  });
 });
 
 module.exports = app;
