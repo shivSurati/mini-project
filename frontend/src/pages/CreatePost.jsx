791ab10dc6e02bb112c6b189c7b4b024af8b6d9c
shivam surati <shivamsurati04@gmail.com>
2026-04-01 16:02:11 +0530

    error handeling

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,42 +1,43 @@
-import React from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
 
 const CreatePost = () => {
-  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const navigate = useNavigate()
 
-    const formData = new FormData(e.target);
+    const handleSubmit = async (e) => {
+        e.preventDefault()
 
-    axios
-      .post("http://localhost:3000/create-post", formData)
-      .then((res) => {
-        navigate("/feed");
-      })
-      .catch((err) => {
-        console.log(err);
-        alert("Error creating post");
-      });
-  };
+        const formData = new FormData(e.target)
 
-  return (
-    <section className="create-post-section">
-      <h1>Create post</h1>
+        axios.post("http://localhost:3000/create-post", formData)
+            .then(() => {
 
-      <form onSubmit={handleSubmit}>
-        <input type="file" name="image" accept="image/*" />
-        <input
-          type="text"
-          name="caption"
-          placeholder="Enter caption"
-          required
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </section>
-  );
-};
+                navigate("/feed")
 
-export default CreatePost;
+            })
+            .catch((err) => {
+                console.log(err)
+                alert("Error creating post")
+            })
+    }
+
+    return (
+        <section className='create-post-section' >
+            <h1>Create post</h1>
+
+            <form onSubmit={handleSubmit} >
+
+                <input type="file" name="image" accept="image/*" />
+                <input type="text" name='caption' placeholder='Enter caption' required />
+                <button type='submit' >Submit</button>
+
+            </form>
+
+        </section>
+    )
+}
+
+export default CreatePost
